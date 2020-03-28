3255d1563c4182697f0a1d5f59191e486e32c897
Sash Patel <bhoolku5443@gmail.com>
2020-03-28 19:48:46 -0400

    Updates

---
+++ precache-manifest.75323a9336d1ade013ea5142b3914bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfd88ca18f8e65abf25e068737c931d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c7dca01aa9c1050673",
+    "url": "/static/css/main.94a5f72e.chunk.css"
+  },
+  {
+    "revision": "fa65b566a0d55ea5838e",
+    "url": "/static/js/2.679b8f53.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/static/js/2.679b8f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c7dca01aa9c1050673",
+    "url": "/static/js/main.a3bcdb35.chunk.js"
+  },
+  {
+    "revision": "0e61984c985f92c781e1",
+    "url": "/static/js/runtime-main.0d515590.js"
+  }
+]);
